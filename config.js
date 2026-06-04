380ca2e5b67262c5ba8168b9d5b23a2b2f276ea7
FunGuys-official <fg.fukuda@funguys.jp>
2026-06-04 17:20:24 +0900

    Update Google Form URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FUNGUYS_LP_CONFIG = {
-  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLSdczGTpyFAMUq5bfIRTAUX66R_m--bFvQAmnUbledp9qd3xrA/viewform?embedded=true",
+  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLSe1M4hMRtiNphjKUkpzMKh8GwyCpobs6bXoJMEJihHtLzfmLQ/viewform?embedded=true",
   lineEntryUrl: "https://lin.ee/XcGyURo",
   lineQrImage: "assets/line-qr-funguys.png",
   lineDisplayName: "FunGuys採用公式LINE",
